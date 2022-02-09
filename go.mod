0d8dfe766cb4f015de1617e56d2a0c7fc0e0b48b
Alexandre Shinebourne <alexandre@shinebourne.com>
2022-02-09 16:27:45 +0000

    switched to retrieval by hash as apposed to by uuid

--- go.mod
+++ go.mod
@@ -3,11 +3,13 @@ module github.com/a-shine/butter
 go 1.17
 
 require (
-	github.com/jordwest/mock-conn v0.0.0-20180617021051-4896c6bd1641 // indirect
-	github.com/nu7hatch/gouuid v0.0.0-20131221200532-179d4d0c4d8d // indirect
-	github.com/pbnjay/memory v0.0.0-20210728143218-7b4eea64cf58 // indirect
+	github.com/jordwest/mock-conn v0.0.0-20180617021051-4896c6bd1641
+	github.com/pbnjay/memory v0.0.0-20210728143218-7b4eea64cf58
+	gitlab.com/NebulousLabs/go-upnp v0.0.0-20211002182029-11da932010b6
+)
+
+require (
 	gitlab.com/NebulousLabs/fastrand v0.0.0-20181126182046-603482d69e40 // indirect
-	gitlab.com/NebulousLabs/go-upnp v0.0.0-20211002182029-11da932010b6 // indirect
 	golang.org/x/crypto v0.0.0-20210322153248-0c34fe9e7dc2 // indirect
 	golang.org/x/net v0.0.0-20210410081132-afb366fc7cd1 // indirect
 	golang.org/x/sys v0.0.0-20210330210617-4fbd30eecc44 // indirect
--- persist/overlay.go
+++ persist/overlay.go
@@ -1,15 +1,16 @@
 package persist
 
 import (
+	"crypto/sha256"
+	"encoding/hex"
 	"errors"
 	"fmt"
 	"github.com/a-shine/butter/node"
-	uuid "github.com/nu7hatch/gouuid"
 )
 
 type Overlay struct {
 	node    *node.Node
-	storage map[uuid.UUID]Block
+	storage map[[32]byte]Block
 }
 
 func (o *Overlay) Node() *node.Node {
@@ -20,25 +21,22 @@ func (o *Overlay) Node() *node.Node {
 // functionality yet!). Returns the UUID of the new block as a string.
 func (o *Overlay) AddBlock(keywords []string, data string) string {
 	// TODO: add the logic to break down the data into blocks if it exceeds the block size
-	id, _ := uuid.NewV4()
-	o.storage[*id] = Block{
+	hsha2 := sha256.Sum256([]byte(data))
+	o.storage[hsha2] = Block{
 		keywords: naiveProcessKeywords(keywords),
 		part:     1,
 		parts:    1,
 		data:     naiveProcessData(data),
 	}
-	return id.String()
+	return fmt.Sprintf("%x", hsha2)
 }
 
 // Block from the node's storage by its UUID. If the block is not found, an empty block with an error is returned.
 func (o *Overlay) Block(id string) (Block, error) {
-	parsedId, err := uuid.ParseHex(id)
-	if err != nil {
-		fmt.Println("Error parsing UUID:", err)
-		return Block{}, err
-	}
-	fmt.Println("Parsed ID: ", parsedId)
-	if val, ok := o.storage[*parsedId]; ok {
+	var hash [32]byte
+	data, _ := hex.DecodeString(id)
+	copy(hash[:], data)
+	if val, ok := o.storage[hash]; ok {
 		return val, nil
 	}
 	return Block{}, errors.New("block not found")
@@ -47,6 +45,6 @@ func (o *Overlay) Block(id string) (Block, error) {
 func NewOverlay(node *node.Node) Overlay {
 	return Overlay{
 		node:    node,
-		storage: make(map[uuid.UUID]Block),
+		storage: make(map[[32]byte]Block),
 	}
 }
